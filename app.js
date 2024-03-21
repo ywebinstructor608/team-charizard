3eb7d025d019855d77e2c5d78566d8952fe30e9b
ywebinstructor608 <162061410+ywebinstructor608@users.noreply.github.com>
2024-03-20 21:25:26 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+// let elm = document.getElementById("GFG");
+
+function Fun_call() {
+  document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+//   elm.innerHTML = "Right click disabled";
+}
